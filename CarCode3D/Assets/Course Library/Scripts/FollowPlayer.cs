a36577492064c4491f2ad4dfa35769b54c215393
Ivan Rodrigues <104801431+IvanRodriguesDev@users.noreply.github.com>
2023-07-30 18:39:28 -0300

    Biul final (arrumar)

--- CarCode3D/Assets/Course Library/Scripts/FollowPlayer.cs
+++ CarCode3D/Assets/Course Library/Scripts/FollowPlayer.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +14,8 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);  
+      transform.Translate(Vector3.forward * Time.deltaTime * 20);  
         
     }
 }
